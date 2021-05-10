c6567e0ee2a9d0b08e71934f7e4afffd3b323ae3
ArkDreal <andrey.o.leal@gmail.com>
2021-05-10 17:33:29 -0300

    Exercicios

---
+++ Exercicio 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Em que ano você nasceu?");
+           int dataNascimento = int.Parse(Console.ReadLine());
+
+           int anoAtual = DateTime.Now.Year;
+
+           int idadeCliente = anoAtual - dataNascimento;
+           
+           if (idadeCliente >= 18){
+               Console.WriteLine("Você pode votar esse ano!");
+           
+            }else{
+               Console.WriteLine("Você não pode votar esse ano!");
+           }
+        }
+    }
+}
